36c6315dc5e72d4cb85647cffc23e3a2dd27d7c9
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-08-26 21:15:58 +0900

    backticks are deprecated, use dollar signs

--- ptrandom.sh
+++ ptrandom.sh
@@ -2,7 +2,7 @@
 RANDOM=$$ #PID of shell is stored in $$ variable
 # RANDOM=$(date +%s)
 NOFR=$((RANDOM*5/32768+2))
-for i in `seq $NOFR`
+for i in $(seq $NOFR)
 do
   echo $((RANDOM*6/32768+1))
 done
