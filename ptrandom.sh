2641ca13f193bdc1feea86eef6538e0487009fb5
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-08-26 21:10:26 +0900

    seq number is now from 2 to 6

--- ptrandom.sh
+++ ptrandom.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 RANDOM=$$ #PID of shell is stored in $$ variable
 # RANDOM=$(date +%s)
-for i in `seq 5`
+NOFR=$((RANDOM*5/32768+2))
+for i in `seq $NOFR`
 do
   echo $((RANDOM*6/32768+1))
 done
