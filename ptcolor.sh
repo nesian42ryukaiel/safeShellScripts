95fb3f386e4dbf5a422e80569df4eb878f8ad5a9
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-08-30 10:24:25 +0900

    grass test

--- ptcolor.sh
+++ ptcolor.sh
@@ -10,3 +10,4 @@ echo -n "\033[40;91mW\033[0m"
 echo " \033[30;107mDISPLAY\033[0m\r\n"
 echo "\033[1;4;40;91mR\033[93mA\033[92mI\033[96mN\033[94mB\033[95mO\033[91mW\033[0m \033[1;4;30;107mDISPLAY\033[0m\r\n"
 echo "\033[41;91mWILD BLAZE\033[0m\r\n" # BG doesn't seem to work with FG
+echo "\033[40;92mGRASS PATCH\033[0m\r\n"
