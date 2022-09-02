f380c6b37cea376792d969a1b3368e6e14558d28
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-09-02 18:44:21 +0900

    grass

---
+++ gitgrass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo
+echo "Github Contribution Simulator"
+echo
+echo "2022-09"
+echo
+echo "\033[40m  \033[0m "
+echo "\033[40m  \033[0m "
+echo "\033[40m  \033[0m "
+echo "\033[40m  \033[0m "
+echo "\033[42m 1\033[0m "
+echo "\033[42m 2\033[0m "
+echo "\033[40m  \033[0m "
+echo
